1a411fe8ed92190e6638009732ed3950162b4de2
Shamimgardobuya <shamimobuya@gmail.com>
2022-05-09 14:34:50 +0300

    dictionary

---
+++ dict.py
@@ -0,0 +1,20 @@
+Python 3.8.10 (default, Mar 15 2022, 12:22:08) 
+[GCC 9.4.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> peace={"Name":12,"Jane":13}
+>>> len(peace)
+2
+>>> type(peace)
+<class 'dict'>
+>>> peace={"Name":12,"Jane":13,"Name":23}
+>>> len(peace)
+2
+>>> #dictionaries does not allow  duplicates
+>>> peace={"Name":12,"Jane":13,"Us":12}
+>>> len(peace)
+3
+>>> #but will recognize of different keys but same value.
+>>> #can dictionaries have also lists as values.
+>>> 
+[1]+  Stopped                 python3
+
